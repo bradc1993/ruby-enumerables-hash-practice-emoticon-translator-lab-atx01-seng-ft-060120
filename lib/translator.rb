7558b1e05a912776856fce820d41d00df49d69c0
Brad Carter <bradc1993@gmail.com>
2020-05-21 07:51:45 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -24,8 +24,7 @@ def get_japanese_emoticon(file_path, emoticon)
       return emoticons[key][:japanese]
     end
   end
- 
-  
+ "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning(file_path, emoticon)
