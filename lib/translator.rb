ab42a731caca7688bf86006715af39d38707a0a2
Brad Carter <bradc1993@gmail.com>
2020-05-21 07:48:03 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -26,7 +26,7 @@ def get_english_meaning(file_path, emoticon)
   # code goes here
   emoticons = load_library(file_path)
   emoticons.each do |k, v|
-    if emoticons[k][v[1]] == emoticon
+    if emoticons[k][:japanese] == emoticon
       return k
     end
     binding.pry
